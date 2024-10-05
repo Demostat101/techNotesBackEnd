27d5771dca998d2a286f17ff10812a488d6bb632
Demostat101 <demostat1995@gmail.com>
2024-10-06 00:17:50 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const routes = require("./routes")
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 4500
+
+// Middleware for parsing JSON
+app.use(express.json());
+app.use(bodyParser.json()); // Correct usage
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", routes);
+// Middleware for parsing URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
