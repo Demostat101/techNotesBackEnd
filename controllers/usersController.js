24a07b1f602f9817e2b735e739405ad4ec311078
Demostat101 <demostat1995@gmail.com>
2024-10-06 18:11:10 +0100

    texting the endpoints with post man

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -12,7 +12,7 @@ const bcrypt = require("bcrypt")
 const getAllUsers = asyncHandler(async(req,res)=>{
     const users = await User.find().select('-password').lean()
 
-    if (!users) {
+    if (!users?.length) {
         return res.status(400).json({message:"No users found"})
     }
 
@@ -65,6 +65,39 @@ const createNewUser = asyncHandler(async(req,res)=>{
 //@access Private
 
 const updateUser = asyncHandler(async(req,res)=>{
+    const {id, username, roles, active,password} = req.body;
+
+    //confirm data
+    if (!id || !username || !password || !Array.isArray(roles) || !roles.length || typeof active !== "boolean") {
+        return res.status(400).json({message:"All fields are required"})
+    }
+
+    const user = await User.findById(id).exec()
+
+    if (!user) {
+        return res.status(400).json({message:"User not found"})
+    }
+
+    // check duplicate
+
+    const duplicate = await User.findOne({username}).lean().exec();
+    //Allow update to the original user
+
+    if (duplicate && duplicate?._id.toString() !== id) {
+        return res.status(409).json({message:"Duplicate username"})
+    }
+
+    user.username = username
+    user.roles = roles
+    user.active = active
+
+    if (password) {
+        user.password = await bcrypt.hash(password,10)
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({message:`${updatedUser.username} updated`})
 
 })
 
@@ -74,6 +107,28 @@ const updateUser = asyncHandler(async(req,res)=>{
 //@access Private
 
 const deleteUser = asyncHandler(async(req,res)=>{
+
+    const {id} = req.body
+
+    if (!id) {
+        return res.status(400).json({message:"User ID Required"})
+    }
+
+    const notes = await Note.findOne({user: id}).lean().exec()
+
+    if (notes?.length) {
+        return res.status(400).json({message:"User has assigned notes"})
+    }
+
+    const user = await User.findById(id).exec()
+    if (!user) {
+        return res.status(400).json({message:"User not found"})
+    }
+
+    const result = await user.deleteOne()
+
+    const reply = `Username ${result.username} with ID ${result._id} deleted`
+    res.json({reply})
 
 })
 
